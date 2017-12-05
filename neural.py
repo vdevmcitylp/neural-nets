bde7c6ecc90ff08cb66214a67b5d187fe6b30221
Vaasudev Narayanan <vaasudev96@gmail.com>
2017-12-05 12:11:58 +0530

    Add files via upload

--- neural.py
+++ neural.py
@@ -3,14 +3,19 @@ import numpy as np
 def sigmoid(x):
 	return 1. / (1 + np.exp(-x))
 
+def loss(Y, output_layer, num_trainset):
+	return np.sum((Y - out[num_layers-1]) ** 2) / num_trainset
+	
 alpha = 0.1
 
 num_layers = 3
 num_trainset = 10
-hidden = [3, 4, 3] # No. of nodes in each layer
+hidden = [3, 4, 2] # No. of nodes in each layer
 
 X = np.random.randn(num_trainset, hidden[0])
 Y = np.random.randn(num_trainset, hidden[-1])
+
+num_epochs = 20
 
 out = []
 net_in = []
@@ -49,32 +54,39 @@ for i in xrange(num_layers-1): # 0 1
 	temp = np.random.randn(hidden[i]+1, hidden[i+1])
 	dtheta.append(temp) #dtheta indices: 1 2 ...
 
-#Forward Propagation
-for i in range(1, num_layers): # 1 2
+for epoch in xrange(num_epochs):
+	print "Epoch: " + str(epoch),
 
-	net_in_bias[i-1] = np.append(out[i-1], np.ones((num_trainset, 1)), axis = 1) 
-	net_in[i] = np.matmul(net_in_bias[i-1], theta[i])
-	out[i] = sigmoid(net_in[i])
+	#Forward Propagation
+	for i in range(1, num_layers): # 1 2
 
-#Backpropagation
+		net_in_bias[i-1] = np.append(out[i-1], np.ones((num_trainset, 1)), axis = 1) 
+		net_in[i] = np.matmul(net_in_bias[i-1], theta[i])
+		out[i] = sigmoid(net_in[i])
+	
+	#Backpropagation
 
-#Error Calculation
-for i in xrange(num_layers-1, 0, -1): # 2 1 
+	#Error Calculation
+	for i in xrange(num_layers-1, 0, -1): # 3 2 1 
 
-	if i == num_layers-1:
-		error[i] = np.mean(out[i] * (1 - out[i]) * (Y - out[i]), axis = 0).reshape(hidden[i], 1)
-	else:
-		error[i] = np.mean(net_in_bias[i] * (1 - net_in_bias[i]) * np.transpose(np.matmul(theta[i+1], error[i+1])), axis = 0).reshape(hidden[i]+1, 1)
-		# Ignore last node
+		if i == num_layers-1:
+			error[i] = np.mean(out[i] * (1 - out[i]) * (Y - out[i]), axis = 0).reshape(hidden[i], 1)
+		elif i == num_layers-2:
+			error[i] = np.mean(net_in_bias[i] * (1 - net_in_bias[i]) * np.transpose(np.matmul(theta[i+1], error[i+1])), axis = 0).reshape(hidden[i]+1, 1)
+			# Ignore last node
+		else:
+			error[i] = np.mean(net_in_bias[i] * (1 - net_in_bias[i]) * np.transpose(np.matmul(theta[i+1], error[i+1][:-1])), axis = 0).reshape(hidden[i]+1, 1)
 
-#Gradient Calculation
-for i in xrange(num_layers-1, 0, -1):
+	#Gradient Calculation
+	for i in xrange(num_layers-1, 0, -1):
 
-	if i == num_layers-1:
-		dtheta[i] = np.matmul(np.transpose(net_in_bias[i-1]), np.repeat(error[i].reshape(1, hidden[i]), num_trainset, axis = 0)) * alpha
-	else:
-		dtheta[i] = np.matmul(np.transpose(net_in_bias[i-1]), np.repeat(error[i][:-1].reshape(1, hidden[i]), num_trainset, axis = 0)) * alpha
+		if i == num_layers-1:
+			dtheta[i] = np.matmul(np.transpose(net_in_bias[i-1]), np.repeat(error[i].reshape(1, hidden[i]), num_trainset, axis = 0)) * alpha
+		else:
+			dtheta[i] = np.matmul(np.transpose(net_in_bias[i-1]), np.repeat(error[i][:-1].reshape(1, hidden[i]), num_trainset, axis = 0)) * alpha
 
-#Weight Update
-for i in xrange(1, num_layers):
-	theta[i] += dtheta[i]
+	#Weight Update
+	for i in xrange(1, num_layers):
+		theta[i] += dtheta[i]
+
+	print "Loss:" + str(loss(Y, out[num_layers-1], num_trainset))
